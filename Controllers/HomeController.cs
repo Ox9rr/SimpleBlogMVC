ee592a72e36843cc8f155fb0387e57c5720d33e6
Ox9rr <619mishka@gmail.com>
2021-08-27 22:30:13 +0300

    fixed fonts to originl state and fixed controller Privacy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace SimpleBlogMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
