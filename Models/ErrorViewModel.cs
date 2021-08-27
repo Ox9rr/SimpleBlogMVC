19e53981bba8ca9854081c6a28022e4a7fb88a26
Ox9rr <619mishka@gmail.com>
2021-08-27 21:50:59 +0300

    added new controller and view and styles for site

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleBlogMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
