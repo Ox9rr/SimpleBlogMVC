dc84c1a35fc6edc35e54ea78958cc0d765210627
Ox9rr <619mishka@gmail.com>
2021-09-14 23:22:12 +0300

    added another admon template and recreated new layout and added new controller GetAllArticles for same view into table type for adminpanel

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
+using SimpleBlogMVC.Models;
 
 namespace SimpleBlogMVC.Controllers
 {
     public class AdminController : Controller
     {
+        IBlog blog;
+        Article article;
+        private readonly ILogger<HomeController> _logger;
+
+        public AdminController(ILogger<HomeController> logger, IBlog b)
+        {
+            _logger = logger;
+            blog = b;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +29,12 @@ namespace SimpleBlogMVC.Controllers
         public IActionResult CreatePost()
         {
             return View();
+        }
+
+        [Route("admin/articles")]
+        public IActionResult GetAllPosts()
+        {
+            return View(blog.GetArticles());
         }
 
         //[Route("admin/create")]
