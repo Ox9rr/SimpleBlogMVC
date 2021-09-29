5523772364d980850649ca24e36c7f390be7fb89
Ox9rr <619mishka@gmail.com>
2021-09-29 20:16:15 +0300

    added 2 new classes. DataBaseLogger ad User

---
+++ DataBaseLogger/DbLoggerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlogMVC.DataBaseLogger
+{
+    public class DbLoggerModel
+    {
+    }
+}
